ea2d6a046a0f61ea8ef434be9493867d6ed1389d
Artanisax <12110524@mail.sustech.edu.cn>
2023-09-20 11:30:09 +0800

    roughly finish Lab Assignment 1

--- Lab1/assignment1.cpp
+++ Lab1/assignment1.cpp
@@ -1,59 +1,63 @@
 #include <iostream>
 #include <cstring>
 
-using std::cout, std::endl;
-
-// enum TOKEN
-// {   // lexemes
-//     TYPE,   // int
-//     INT,    // integer
-//     ID,     // {L, _, $}{L, D, _, $}*
-//     IF,     // if
-//     ELSE,   // else
-//     WHILE,  // while
-//     RET,    // return
-//     SEMI,   // ;
-//     ASSIGN, // =
-//     LT,     // <
-//     LE,     // <=
-//     GT,     // >
-//     GE,     // >=
-//     NE,     // !=
-//     EQ,     // ==
-//     PLUS,   // +
-//     MINUS,  // -
-//     LP,     // (
-//     RP,     // )
-//     LC,     // {
-//     RC,     // }
-// };
-
-// const char *TOKEN_NAME[] =
-// {
-//     "TYPE",
-//     "INT",
-//     "ID",
-//     "IF",
-//     "ELSE",
-//     "WHILE",
-//     "RET",
-//     "SEMI",
-//     "ASSIGN",
-//     "LT",
-//     "LE",
-//     "GT",
-//     "GE",
-//     "NE",
-//     "EQ",
-//     "PLUS",
-//     "MINUS",
-//     "LP",
-//     "RP",
-//     "LC",
-//     "RC",
-// };
+using std::cout, std::endl, std::min;
 
 #define MAX_LEN 100
+
+const size_t SIGN_NUM = 14, SIGN_LEN = 2;
+const char *KEY_SIGN[] =
+{
+    ";",
+    "=",
+    "<",
+    "<=",
+    ">",
+    ">=",
+    "!=",
+    "==",
+    "+",
+    "-",
+    "(",
+    ")",
+    "{",
+    "}",
+};
+const char *SIGN_NAME[] =
+{
+    "SEMI",
+    "ASSIGN",
+    "LT",
+    "LE",
+    "GT",
+    "GE",
+    "NE",
+    "EQ",
+    "PLUS",
+    "MINUS",
+    "LP",
+    "RP",
+    "LC",
+    "RC",
+};
+
+const size_t WORD_NUM = 5, WORD_LEN = 6;
+const char *KEY_WORD[] =
+{
+    "int",
+    "if",
+    "else",
+    "while",
+    "return",
+};
+const char *WORD_NAME[] =
+{
+    "TYPE",
+    "IF",
+    "ELSE",
+    "WHILE",
+    "RET"
+};
 
 enum STATE
 {
@@ -63,6 +67,8 @@ enum STATE
     LETTER,
 };
 
+bool is_empty(char x)
+{ return x == ' '; }
 
 bool is_sign(char x)
 {
@@ -79,36 +85,82 @@ bool is_letter(char x)
 bool end_of_line(char x)
 { return x == '\n' || x == '\r' || x == '\0'; }
 
+void print_sign(char *s, size_t len)
+{
+    for (size_t start = 0, length = min(SIGN_LEN, len); start < len; length--)
+        for (int i = 0; i < SIGN_NUM; i++)
+            if (strncmp(s + start, KEY_SIGN[i], length) == 0)
+            {
+                cout << SIGN_NAME[i] << ' ';
+                start += length;
+                length = min(SIGN_LEN, len - start);
+            }
+}
+
+void print_word(char *s, size_t len)
+{
+    if (len <= WORD_LEN)
+        for (size_t i = 0; i < WORD_NUM; i++)
+            if (strncmp(s, KEY_WORD[i], len) == 0)
+            {
+                cout << WORD_NAME[i] << ' ';
+                return;
+            }
+    cout << "ID ";
+}
+
 void analyze(FILE *f)
 {
     char *line = new char[MAX_LEN];
     while (fgets(line, MAX_LEN, f))
     {
-        size_t st = 0;
+        size_t start = 0;
         for (size_t p = 0, state = STATE::EMPTY; !end_of_line(line[p]); p++)
         {
             char ch = line[p];
+            if (!is_empty(ch) && !is_sign(ch) && !is_digit(ch) && !is_letter(ch))
+                goto ERROR;
             switch (state)
             {
                 case STATE::EMPTY:
                     if (is_sign(ch))
                     {
-                        st = p;
+                        start = p;
                         state = STATE::SIGN;
                     }
                     if (is_digit(ch))
-                    {
                         state = STATE::DIGIT;
+                    if (is_letter(ch))
+                    {
+                        start = p;
+                        state = STATE::LETTER;
                     }
                     break;
                 case STATE::SIGN:
-                    
+                    if (!is_sign(ch))
+                    {
+                        print_sign(line + start, p - start);
+                        p--;
+                        state = STATE::EMPTY;
+                    }
                     break;
                 case STATE::DIGIT:
-                    
+                    if (!is_digit(ch))
+                    {
+                        if (is_letter(ch))
+                            goto ERROR;
+                        cout << "INT ";
+                        p--;
+                        state = STATE::EMPTY;
+                    }
                     break;
                 case STATE::LETTER:
-                    
+                    if (!is_letter(ch) && !is_digit(ch))
+                    {
+                        print_word(line + start, p - start);
+                        p--;
+                        state = STATE::EMPTY;
+                    }
                     break;
                 default:
                     goto ERROR;
